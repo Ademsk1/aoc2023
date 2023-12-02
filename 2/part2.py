a590e2c8f1c8376349dc029bc66b06d8b5da3565
Ademsk1 <adam.illoul24@gmail.com>
2023-12-02 22:28:25 +0000

    refactor part2

--- 2/part2.py
+++ 2/part2.py
@@ -8,17 +8,21 @@ def read(file):
     return rawdata
 
 
+def multiplier(entry):
+    multiplier = 1
+    colours = ['red', 'green', 'blue']
+    for colour in colours:
+        max_colour = max([int(c)
+                          for c in re.findall(f'\d+(?= {colour})', entry)])
+        multiplier *= max_colour
+    s += multiplier
+
+
 def main(file):
     rawdata = read(file)
     s = 0
-    colours = ['red', 'green', 'blue']
     for entry in rawdata:
-        multiplier = 1
-        for colour in colours:
-            max_colour = max([int(c)
-                             for c in re.findall(f'\d+(?= {colour})', entry)])
-            multiplier *= max_colour
-        s += multiplier
+        s += multiplier(entry)
     print(s)
 
 
