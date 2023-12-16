ee82a3fa8d7c255995839515523949b49b03fc7a
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-12-16 15:37:02 +0000

    day 8 part1

---
+++ 8/part1.py
@@ -0,0 +1,39 @@
+import re
+def read(file):
+    with open(file) as f:
+        rawdata = f.readlines()
+
+    return rawdata
+
+
+
+def main(file):
+	rawdata = read(file)
+	instructions, maps = rawdata[0].strip(), [line.strip() for line in rawdata[2:]]
+	print(instructions)
+	print(maps)
+	starting, ending = 'AAA', 'ZZZ'
+	current = starting
+	i = 0
+	k = 0
+	instruction_index = {
+		'L': 0,
+		'R': 1
+	}
+	while current != ending:
+		if i == len(instructions):
+			i = 0
+		direction = instruction_index[instructions[i]]
+		location = re.findall(f'{current} = \([A-Z]+, [A-Z]+\)', '\n'.join(rawdata))[0]
+		available_locations = re.findall('[A-Z]+',location.split(' = ')[1])
+		print(available_locations)
+		current = available_locations[direction]
+		print(current)
+		i+=1
+		k+=1
+		
+	print(k)
+
+
+
+main('./8/data.txt')
